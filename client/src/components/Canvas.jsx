6791270c61fe1ece60ed56c1fbd269634e8bcd82
nikita_romanovich1@bat.com <nikita_romanovich1@bat.com>
2023-02-21 14:36:35 +0300

    first commit client

---
+++ client/src/components/Canvas.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import '../styles/canvas.scss'
+const Canvas = () => {
+    return (
+        <div className="canvas">
+            <canvas width={800} height={600} />
+        </div>
+    )
+}
+export default Canvas;
